7468702881cf3fa62f2740e9e9bb853a483e38d5
michael tamsil <michaeltamsil@gmail.com>
2022-10-29 10:40:35 +0700

    set text value ke label

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabelValue: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var weightSliderValue: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        heightSliderChanged(heightSlider)
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabelValue.text = String(format: "%.1f",sender.value) + " m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightSliderValue.text = String(Int(sender.value)) + " Kg"
+    }
 }
 

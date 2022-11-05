3e186a2567a1b6b19fe1c96a4463d28c9fa1bb68
michael tamsil <michaeltamsil@gmail.com>
2022-11-05 12:43:10 +0700

    lempar value ke tampilan yg lain via segue

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
 
     @IBOutlet weak var heightLabelValue: UILabel!
     @IBOutlet weak var heightSlider: UISlider!
     
     @IBOutlet weak var weightSliderValue: UILabel!
     @IBOutlet weak var weightSlider: UISlider!
+    
+    var bmi: Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,8 +35,17 @@ class ViewController: UIViewController {
     @IBAction func calculatePressed(_ sender: UIButton) {
         let height = heightSlider.value
         let weight = weightSlider.value
-        let bmi = weight / pow(height, 2)
-        print(bmi)
+        bmi = weight / pow(height, 2)
+        
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = String(format: "%.1f", bmi)
+        }
     }
 }
 
---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by michael tamsil on 05/11/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        resultLabel.text = bmiValue
+    }
+    
+
+    @IBAction func recalculatePressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

fef16084fed9f5e38fce02ca5517cce385b7e7d9
michael tamsil <michaeltamsil@gmail.com>
2022-11-06 18:57:14 +0700

    bisa rubah background color based on bobot berat

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -11,15 +11,20 @@ import UIKit
 class ResultViewController: UIViewController {
     
     var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
 
     
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         resultLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
     }
     
 
--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -7,15 +7,40 @@
 //
 
 import Foundation
+import UIKit
 
 struct CalculatorBrain {
-    var BMIValue: Float?
+    var BMIValue: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float){
-        BMIValue = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+        switch bmiValue {
+        case _ where bmiValue < 18.5:
+            let color = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+            BMIValue = BMI(value: bmiValue, advice: "Eat more pies!", color: color )
+            break
+        case 18.5..<24.9:
+            let color = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+            BMIValue = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: color )
+            break
+        case _ where bmiValue >= 24.9:
+            let color = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+            BMIValue = BMI(value: bmiValue, advice: "Eat less pies!", color: color )
+            break
+        default: break
+            
+        }
     }
     
     func getBMIValue() -> Float{
-        return BMIValue!
+        return BMIValue?.value ?? 0.0
+    }
+    
+    func getAdvice() -> String {
+        return BMIValue?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return BMIValue?.color ??  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
 }
